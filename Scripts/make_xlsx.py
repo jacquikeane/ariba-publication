1a5d380244847cac6125cd06a13331f91fbe1eb4
martinghunt <martin.g.hunt@gmail.com>
2017-04-18 14:49:27 +0100

    Create missing empty directories

--- Scripts/make_xlsx.py
+++ Scripts/make_xlsx.py
@@ -5,13 +5,13 @@ import openpyxl
 
 filenames_to_load = [
     'E_faecium/summary.supplementary_table.tsv',
-    'Time_and_memory/resources.tsv',
     'S_sonnei/shigella_supplementary_holt2012.tsv',
     'S_sonnei/Ref/ref_data.in.tsv',
-    'S_sonnei/summary.calls.tsv',
+    'S_sonnei/summary.amr_genes.tsv',
     'N_gonorrhoeae/fig5.mic_data.csv',
     'N_gonorrhoeae/suppl_table_gono_antibios.xlsx',
-    'S_sonnei/summary.amr_genes.tsv',
+    'Time_and_memory/resources.tsv',
+    'S_sonnei/summary.calls.tsv',
 ]
 
 
